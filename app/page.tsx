d2ba14287fdc0ed8e898a3ebd26526f36b5818cb
Dmitry Jum <xfiler@ymail.com>
2024-07-27 02:46:23 -0700

    adds welcome card for logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import { Button } from '@/components/ui/button';
+import SignInButton from '@/components/SignInButton';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { getAuthSession } from '@/lib/nextauth';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession()
+  if (session?.user) {
+    redirect('/dashboard')
+  }
   return (
-    <Button>Hello World</Button>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className='2-[300px]'>
+        <CardHeader>
+          <CardTitle>Welcome to Intelli Casino</CardTitle>
+          <CardDescription>
+            Intelli Casino is a quiz app that allows you to place bets on the quiz app as a dealer of the quiz or on the player who's taking it or be the one to play it.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign in with Google!" />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
