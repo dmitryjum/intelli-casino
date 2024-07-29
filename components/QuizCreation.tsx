d5262a069682854d8cd7a40ca1d20a51bff29620
Dmitry Jum <xfiler@ymail.com>
2024-07-28 20:39:08 -0700

    form building for quiz creation is in progress

--- components/QuizCreation.tsx
+++ components/QuizCreation.tsx
@@ -1,9 +1,41 @@
+'use client'
 import React from 'react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { useForm } from 'react-hook-form';
+import { quizCreationSchema } from '@/schemas/form/quiz';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 type Props = {}
 
+type Input = z.infer<typeof quizCreationSchema>
+
 const QuizCreation = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "open_ended"
+    },
+  });
+
+  function onSubmit(input: Input) {
+    alert(JSON.stringify(input, null, 2))
+  }
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card>
@@ -11,7 +43,49 @@ const QuizCreation = (props: Props) => {
           <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
           <CardDescription>Choose a topic</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter a topic..." {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Please provide a topic
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of questions</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter an amount..."
+                       {...field}
+                       type="number"
+                       min={1}
+                       max={10}
+                       onChange={e => {
+                       form.setValue("amount", parseInt(e.target.value));
+                      }} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   )
