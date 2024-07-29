8b604030e01bd948efae8a837c71e8040bdb4d27
Dmitry Jum <xfiler@ymail.com>
2024-07-28 19:25:10 -0700

    adds quiz creation container

---
+++ app/quiz/page.tsx
@@ -0,0 +1,20 @@
+import QuizCreation from '@/components/QuizCreation'
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {}
+
+export const metadata = {
+  title: "Quiz | Intelli Casino"
+}
+
+const QuizPage = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect('/')
+  }
+  return <QuizCreation />
+}
+
+export default QuizPage
