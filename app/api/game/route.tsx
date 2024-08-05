e9cb8140ae8d92ec68a6fceeac014063d04a409f
Dmitry Jum <xfiler@ymail.com>
2024-08-05 14:12:57 -0700

    game api end point generates a new game, set of questions, sets the questions to the game and returns the game id

--- app/api/game/route.tsx
+++ app/api/game/route.tsx
@@ -3,6 +3,7 @@ import { getAuthSession } from "@/lib/nextauth";
 import { quizCreationSchema } from "@/schemas/form/quiz";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+import axios from "axios";
 
 // /api/game
 export async function POST(req: Request, res: Response) {
@@ -14,7 +15,7 @@ export async function POST(req: Request, res: Response) {
       }, { status: 401 });
     }
     const body = await req.json();
-    const {amount, topic, type} = quizCreationSchema.parse(body);
+    const {amount, topic, type} = quizCreationSchema.parse(body); // fits the request params into quiz creation schema and destructures it to create a new Game db record
     const game = await prisma.game.create({
       data: {
         gameType: type,
@@ -22,7 +23,57 @@ export async function POST(req: Request, res: Response) {
         userId: session.user.id,
         topic
       }
-    })
+    });
+
+    const {data} = await axios.post(`${process.env.API_URL}/api/questions`, { // gets a set of questions from Open AI
+      amount,
+      topic,
+      type
+    });
+
+    if (type === "mcq") {
+      type mcqQuestion = {
+        question: string,
+        answer: string,
+        option1: string,
+        option2: string,
+        option3: string,
+      }
+      let manyData = data.questions.map((question: mcqQuestion) => {
+        let options = [question.answer, question.option1, question.option2, question.option3];
+        options = options.sort(() => Math.random() - 0.5);
+        return {
+          question: question.question,
+          answer: question.answer,
+          options: JSON.stringify(options),
+          gameId: game.id, // comes from recently created game db recrod above
+          questionType: 'mcq'
+        }
+      })
+
+      await prisma.question.createMany({
+        data: manyData
+      })
+    } else if (type === "open_ended") {
+      type openQuestion = {
+        question: string,
+        answer: string,
+      }
+      let manyData = data.questions.map((question: openQuestion) => {
+        return {
+          question: question.question,
+          answer: question.answer,
+          gameId: game.id,
+          questionType: 'open_ended'
+        }
+      })
+      await prisma.question.createMany({
+        data: manyData
+      })
+    }
+    return NextResponse.json({
+      gameId: game.userId
+    });
   } catch (error: any) {
     if (error instanceof ZodError) {
       return NextResponse.json({ error: error.issues }, { status: 400 });
