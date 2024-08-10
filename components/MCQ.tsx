e111283f3d0bcc9182532e427dc63f13c9044f9e
Dmitry Jum <xfiler@ymail.com>
2024-08-10 15:09:22 -0700

    MCQ page runs the timer

--- components/MCQ.tsx
+++ components/MCQ.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Game, Question } from '@prisma/client'
+import { differenceInSeconds } from 'date-fns'
 import { BarChart, ChevronRight, Loader2, Timer } from 'lucide-react'
 import React from 'react'
 import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
@@ -11,7 +12,7 @@ import { z } from 'zod'
 import { checkAnswerSchema } from '@/schemas/form/quiz'
 import { useToast } from './ui/use-toast'
 import Link from 'next/link'
-import { cn } from '@/lib/utils'
+import { cn, formatTimeDelta } from '@/lib/utils'
 
 type Props = {
   game: Game & {questions: Pick<Question, 'id' | 'options' | 'question'>[]}
@@ -24,6 +25,15 @@ const MCQ = ({game}: Props) => {
   const [wrongAnswers, setWrongAnswers] = React.useState<number>(0);
   const [hasEnded, setHasEnded] = React.useState<boolean>(false);
   const {toast} = useToast();
+  const [now, setNow] = React.useState<Date>(new Date());
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (!hasEnded) {
+        setNow(new Date());
+      }
+    }, 1000);
+    return () => { clearInterval(interval) }
+  }, [hasEnded])
 
   const currentQuestion = React.useMemo(() => {
     return game.questions[questionIndex]
@@ -98,7 +108,7 @@ const MCQ = ({game}: Props) => {
     return (
       <div className="absolute flex flex-col justify-center top-1/2 left-1/2 -translate-x-1/2 top-1/2 left-1/2">
         <div className="px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap">
-          You completed in {'3min 4s'}
+          You completed in {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
         </div>
         <Link href={`/statistics/${game.id}`} className={cn(buttonVariants(), "mt-2")}>
           View Statistics
@@ -118,7 +128,7 @@ const MCQ = ({game}: Props) => {
           </p>
           <div className="flex self-start mt-3 text-slate-400">
             <Timer className="mr-2" />
-            <span>00:00</span>
+            {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
           </div>
         </div>
         <MCQCounter correctAnswers={correctAnswers} wrongAnswers={wrongAnswers} />
