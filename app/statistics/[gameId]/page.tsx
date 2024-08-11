d3f53a57a6473b1acdb30327408701894b6c879d
Dmitry Jum <xfiler@ymail.com>
2024-08-11 13:36:52 -0700

    stats page shows the results reward card if the accuracy is over 75%

--- app/statistics/[gameId]/page.tsx
+++ app/statistics/[gameId]/page.tsx
@@ -1,5 +1,9 @@
+import ResultsCard from '@/components/statistics/ResultsCard';
+import { buttonVariants } from '@/components/ui/button';
 import { prisma } from '@/lib/db';
 import { getAuthSession } from '@/lib/nextauth';
+import { LucideLayoutDashboard } from 'lucide-react';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
@@ -19,7 +23,26 @@ const StatisticsPage = async ({params: {gameId}}: Props) => {
   if (!game) { return redirect("/quiz") };
 
   return (
-    <div>{gameId}</div>
+    <>
+      <div className="px-8 mx-auto max-w-7xl">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Statistics</h2>
+          <div className="flex items-center space-x-2">
+            <Link href='/dashboard' className={buttonVariants()}>
+              <LucideLayoutDashboard className='mr-2' />
+              Back to Dashboard
+            </Link>
+          </div>
+        </div>
+        <div className="grid gap-4 mt-4 md:grid-cols-7">
+          <ResultsCard accuracy={76}/>
+          {/* <AccuracyCard /> */}
+          {/* <TimeTakenCard /> */}
+        </div>
+
+        {/* <QuestionList /> */}
+      </div>
+    </>
   )
 }
 
