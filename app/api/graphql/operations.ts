0e8393ac24a650f1f057addbc834b0fb937bee3f
Dmitry Jum <xfiler@ymail.com>
2024-10-06 22:06:36 -0700

    refactors graphql operations with fragmens

--- app/api/graphql/operations.ts
+++ app/api/graphql/operations.ts
@@ -1,154 +1,65 @@
 import { gql } from "@apollo/client";
+import { GAME_FRAGMENT } from "./fragments";
 
 export const GET_ACTIVE_GAMES = gql`
   query GetActiveGames {
     activeGames {
-      id
-      status
-      topic
-      gameType
-      openAt
-      userId
-      timeStarted
-      timeEnded
+     ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
 
 export const GET_GAME = gql`
   query GetGame($gameId: String!) {
     game(gameId: $gameId) {
-      id
-      status
-      topic
-      userId
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `
 
 export const GAME_UPDATED = gql`
   subscription onGameUpdated($gameId: String) {
     gameUpdated(gameId: $gameId) {
-     id
-      status
-      topic
-      userId
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
 
 export const OPEN_GAME = gql`
   mutation OpenGame($gameId: String!, $currentQuestionStartTime: DateTime, $currentQuestionIndex: Int) {
     openGame(gameId: $gameId, currentQuestionStartTime: $currentQuestionStartTime, currentQuestionIndex: $currentQuestionIndex) {
-      id
-      status
-      topic
-      userId
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
 
 export const CLOSE_GAME = gql`
   mutation CloseGame($gameId: String!, $currentQuestionStartTime: DateTime, $currentQuestionIndex: Int) {
     closeGame(gameId: $gameId, currentQuestionStartTime: $currentQuestionStartTime, currentQuestionIndex: $currentQuestionIndex) {
-      id
-      status
-      topic
-      userId
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
 
 export const FINISH_GAME = gql`
   mutation FinishGame($gameId: String!, $timeEnded: DateTime) {
     finishGame(gameId: $gameId, timeEnded: $timeEnded) {
-      id
-      topic
-      userId
-      status
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
 
 export const UPDATE_GAME_QUESTION = gql`
   mutation UpdateGameQuestion($gameId: String!, $currentQuestionStartTime: DateTime!, $currentQuestionIndex: Int!) {
     updateGameQuestion(gameId: $gameId, currentQuestionStartTime: $currentQuestionStartTime, currentQuestionIndex: $currentQuestionIndex) {
-      id
-      status
-      topic
-      userId
-      openAt
-      gameType
-      timeStarted
-      timeEnded
-      currentQuestionIndex
-      currentQuestionStartTime
-      questions {
-        id
-        question
-        options
-        answer
-      }
+      ...GameFields
     }
   }
+  ${GAME_FRAGMENT}
 `;
